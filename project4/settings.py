cf73a781b2f1139344c3b567ceb1014f7c482095
Larry Miller <musicmrmiller@gmail.com>
2021-07-05 09:23:57 -0400

    adjust settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/network/static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
