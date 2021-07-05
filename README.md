982b73fead55d96767e49123c933807ea560d0a2
Larry Miller <musicmrmiller@gmail.com>
2021-07-04 23:37:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a social networking web app called Vocalize, developed by Larry Miller. It was created with Python (Django), JavaScript, and HTML/CSS (including the Bootstrap library). It also utilizes JSON to handle information for liking posts and following users by making fetch requests to a custom API.
--- project4/settings.py
+++ project4/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ALLOWED_HOSTS = ['127.0.0.1','https://vocalizeapp.herokuapp.com/']]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
